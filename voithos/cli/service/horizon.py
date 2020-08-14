04f20a637cf7f934bab4a59eacf77702ab159022
kylep <kyle@pericak.com>
2020-08-14 15:01:33 +0000

    Fixed horizon

--- voithos/cli/service/horizon.py
+++ voithos/cli/service/horizon.py
@@ -11,17 +11,24 @@ import voithos.lib.service.horizon as horizon
 
 @click.option("--ip", "ip_address", default="0.0.0.0", help="[optional] bind IP address")
 @click.option("--port", default="80", help="[optional] bind port (default 80)")
-@click.option("--openstack-fqdn", "openstack_fqdn", required=True, help="OpenStack VIP")
+@click.option("--internal-vip", "internal_vip", required=True, help="Internal OpenStack VIP")
+@click.option("--control-node-ip", "control_node_ip", required=True, help="IP of a control node running memcached")
 @click.option(
-    "--keystone-url",
-    "keystone_url",
-    required=True,
-    help="Full keystone URL. Example: http://preview.breqwatr.com:5000/v3",
+    "--conf-dir", "conf_dir", default="/etc/kolla/horizon", help="[optional] Config dir override"
 )
+@click.option("--name", default="horizon", help="[optional] Container name override")
+@click.option("--release", default="train", help="[optional] Docker image tag override")
 @click.command()
-def start(ip_address, port, openstack_fqdn, keystone_url):
+def start(ip_address, port, internal_vip, control_node_ip, conf_dir, name, release):
     """ Launch the Horizon service """
-    horizon.start(ip_address, port, openstack_fqdn, keystone_url)
+    horizon.start(
+        ip_address=ip_address,
+        port=port,
+        internal_vip=internal_vip,
+        control_node_ip=control_node_ip,
+        release=release,
+        conf_dir=conf_dir,
+        name=name)
 
 
 def get_horizon_group():
